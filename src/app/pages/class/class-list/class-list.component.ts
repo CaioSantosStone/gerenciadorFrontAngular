0b4b3a0c14b9e309394e6d99deb664cc125306ea
Caio Santos <caio.michel@gmail.com>
2018-10-02 00:52:08 -0300

    Front para cadastro de aula

--- src/app/pages/class/class-list/class-list.component.ts
+++ src/app/pages/class/class-list/class-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ClassService } from '../../../services/class.service';
+import { LocalDataSource } from 'ng2-smart-table';
+
 
 @Component({
   selector: 'ngx-class-list',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./class-list.component.scss']
 })
 export class ClassListComponent implements OnInit {
+  //Controle da SmartTable
+  settings
+  source: LocalDataSource = new LocalDataSource();
 
-  constructor() { }
+  constructor(
+    private classService: ClassService
+  ) { }
+
+
 
   ngOnInit() {
+    this.settings = this.classService.getGridConfig();
+    this.loadContents()
   }
+  async loadContents() {
+    try {
+      let teste = [{
+        _id: "dsfsfsdfsdfdsfdsfdsf",
+        dayAndHors: "27/10/2018",
+        title: "BodyJump",
+        type: "Fechada",
+      }]
+      this.source.load(teste);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
 
 }
